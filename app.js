3aeba5d9739814d2a9122f26325c2227deb60351
Fred Hobbs <FHobbs8030@Gmail.com>
2025-07-25 12:18:55 -0700

    Add temporary auth middleware with test user ID

--- app.js
+++ app.js
@@ -6,7 +6,18 @@ const { PORT = 3001 } = process.env;
 
 const app = express();
 
-mongoose.connect('mongodb://127.0.0.1:27017/wtwr_db');
+mongoose.connect('mongodb://127.0.0.1:27017/wtwr_db', {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
+
+// ✅ Temporary authorization middleware (with test user ID)
+app.use((req, res, next) => {
+  req.user = {
+    _id: '5d8b8592978f8bd833ca8133', // Use actual test user's _id here
+  };
+  next();
+});
 
 app.use(express.json());
 app.use(routes);
