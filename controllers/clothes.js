c776455f33de6b79248d6c1b53fbd1482de5320a
Fred Hobbs <FHobbs8030@Gmail.com>
2025-07-29 12:48:33 -0700

    modify

--- controllers/clothes.js
+++ controllers/clothes.js
@@ -2,11 +2,79 @@ const ClothingItem = require('../models/clothingItem');
 const mongoose = require('mongoose');
 const {
   STATUS_OK,
+  STATUS_CREATED,
   STATUS_BAD_REQUEST,
-  STATUS_INTERNAL_SERVER_ERROR,
   STATUS_NOT_FOUND,
+  STATUS_INTERNAL_SERVER_ERROR,
 } = require('../utils/constants');
 
+// Get all clothing items
+module.exports.getClothingItems = (req, res) => {
+  ClothingItem.find({})
+    .then((items) => res.status(STATUS_OK).send(items))
+    .catch(() =>
+      res.status(STATUS_INTERNAL_SERVER_ERROR).send({ message: 'Server error while retrieving items' })
+    );
+};
+
+// Create a new clothing item
+module.exports.createClothingItem = (req, res) => {
+  const { name, imageUrl, weather } = req.body;
+  const owner = req.user._id;
+
+  ClothingItem.create({ name, imageUrl, weather, owner })
+    .then((item) => res.status(STATUS_CREATED).send(item))
+    .catch((err) => {
+      if (err.name === 'ValidationError') {
+        return res.status(STATUS_BAD_REQUEST).send({ message: err.message });
+      }
+      return res.status(STATUS_INTERNAL_SERVER_ERROR).send({ message: 'Server error while creating item' });
+    });
+};
+
+// Like a clothing item
+module.exports.likeItem = (req, res) => {
+  if (!mongoose.Types.ObjectId.isValid(req.params.id)) {
+    return res.status(STATUS_BAD_REQUEST).send({ message: 'Invalid item ID' });
+  }
+
+  ClothingItem.findByIdAndUpdate(
+    req.params.id,
+    { $addToSet: { likes: req.user._id } },
+    { new: true }
+  )
+    .orFail(() => new Error('NotFound'))
+    .then((item) => res.status(STATUS_OK).send(item))
+    .catch((err) => {
+      if (err.message === 'NotFound') {
+        return res.status(STATUS_NOT_FOUND).send({ message: 'Item not found' });
+      }
+      return res.status(STATUS_INTERNAL_SERVER_ERROR).send({ message: 'Server error while liking item' });
+    });
+};
+
+// Unlike a clothing item
+module.exports.unlikeItem = (req, res) => {
+  if (!mongoose.Types.ObjectId.isValid(req.params.id)) {
+    return res.status(STATUS_BAD_REQUEST).send({ message: 'Invalid item ID' });
+  }
+
+  ClothingItem.findByIdAndUpdate(
+    req.params.id,
+    { $pull: { likes: req.user._id } },
+    { new: true }
+  )
+    .orFail(() => new Error('NotFound'))
+    .then((item) => res.status(STATUS_OK).send(item))
+    .catch((err) => {
+      if (err.message === 'NotFound') {
+        return res.status(STATUS_NOT_FOUND).send({ message: 'Item not found' });
+      }
+      return res.status(STATUS_INTERNAL_SERVER_ERROR).send({ message: 'Server error while unliking item' });
+    });
+};
+
+// Delete a clothing item
 module.exports.deleteClothingItem = (req, res) => {
   const { id } = req.params;
 
@@ -22,8 +90,6 @@ module.exports.deleteClothingItem = (req, res) => {
       res.status(STATUS_OK).send({ message: 'Item deleted successfully' });
     })
     .catch(() =>
-      res.status(STATUS_INTERNAL_SERVER_ERROR).send({
-        message: 'Server error while deleting item',
-      })
+      res.status(STATUS_INTERNAL_SERVER_ERROR).send({ message: 'Server error while deleting item' })
     );
 };
