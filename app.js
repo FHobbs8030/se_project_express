34421a45928f6091cddffdc2422ed913f3bbb24b
Fred Hobbs <FHobbs8030@Gmail.com>
2025-07-29 14:23:09 -0700

    modify

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const mongoose = require('mongoose');
+const cors = require('cors');
 const routes = require('./routes');
 
 const { PORT = 3001 } = process.env;
@@ -7,6 +8,8 @@ const { PORT = 3001 } = process.env;
 const app = express();
 
 mongoose.connect('mongodb://127.0.0.1:27017/wtwr_db');
+
+app.use(cors());  
 
 app.use((req, res, next) => {
   req.user = {
