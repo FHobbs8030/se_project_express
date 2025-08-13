68a569b0dbac4067789abe93fb66b244387a3dfd
Fred Hobbs <FHobbs8030@Gmail.com>
2025-08-13 16:37:46 -0700

    swap the regexes for validator helpers

--- models/user.js
+++ models/user.js
@@ -1,40 +1,50 @@
 import mongoose from 'mongoose';
+import validator from 'validator';
 
-const userSchema = new mongoose.Schema({
-  name: {
-    type: String,
-    required: true,
-    minlength: 2,
-    maxlength: 30,
-  },
-  avatar: {
-    type: String,
-    required: true,
-    validate: {
-      validator(v) {
-        return /^(https?:\/\/)(www\.)?[\w\-._~:/?#[\]@!$&'()*+,;=]+#?$/.test(v);
+const userSchema = new mongoose.Schema(
+  {
+    name: {
+      type: String,
+      required: true,
+      minlength: 2,
+      maxlength: 30,
+      trim: true,
+    },
+    avatar: {
+      type: String,
+      required: true,
+      validate: {
+        validator(v) {
+          return validator.isURL(v, {
+            require_protocol: true,
+            protocols: ['http', 'https'],
+          });
+        },
+        message: 'Invalid URL for avatar',
       },
-      message: 'Invalid URL for avatar',
+    },
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+      lowercase: true,
+      trim: true,
+      validate: {
+        validator(v) {
+          return validator.isEmail(v);
+        },
+        message: 'Invalid email address',
+      },
+      index: true,
+    },
+    password: {
+      type: String,
+      required: true,
+      select: false,
     },
   },
-  email: {
-    type: String,
-    required: true,
-    unique: true,
-    lowercase: true,
-    validate: {
-      validator(v) {
-        return /^[\w-.]+@([\w-]+\.)+[\w-]{2,4}$/.test(v);
-      },
-      message: 'Invalid email address',
-    },
-  },
-  password: {
-    type: String,
-    required: true,
-    select: false,
-  },
-}, { versionKey: false, timestamps: true });
+  { versionKey: false, timestamps: true },
+);
 
 const User = mongoose.model('User', userSchema);
 
