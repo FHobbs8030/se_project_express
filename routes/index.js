8c5ffee6fd28706eead2d580d3beee96347f8e43
Fred Hobbs <FHobbs8030@Gmail.com>
2025-09-01 09:23:26 -0700

    modify

--- routes/index.js
+++ routes/index.js
@@ -1,14 +1,31 @@
-import express from 'express';
-import itemsRouter from './items.js';
+import { Router } from 'express';
+
+import auth from '../middlewares/auth.js';
+import { login, createUser } from '../controllers/auth.js';
+import { getItems } from '../controllers/items.js';
 import usersRouter from './users.js';
-import { STATUS_NOT_FOUND } from '../utils/constants.js';
+import itemsRouter from './items.js';
 
-const router = express.Router();
+import { STATUS_OK, STATUS_NOT_FOUND } from '../utils/constants.js';
 
-router.use('/items', itemsRouter);
+const router = Router();
+
+// ---- Public routes ----
+router.get('/', (_req, res) => {
+  res.status(STATUS_OK).send({ status: 'ok', routes: ['/signin', '/signup', '/items', '/users/me'] });
+});
+
+router.post('/signin', login);
+router.post('/signup', createUser);
+router.get('/items', getItems);
+
+// ---- Protected routes ----
+router.use(auth);
 router.use('/users', usersRouter);
+router.use('/items', itemsRouter);
 
-router.use((req, res) => {
+// ---- 404 for unknown paths ----
+router.use('*', (_req, res) => {
   res.status(STATUS_NOT_FOUND).send({ message: 'Requested resource not found' });
 });
 
