22992e974044b109eddf73d0163172c087cd507c
Fred Hobbs <FHobbs8030@Gmail.com>
2025-09-01 09:31:45 -0700

    remove this file

--- routes/auth.js
+++
@@ -1,9 +0,0 @@
-import { Router } from 'express';
-import { createUser, login } from '../controllers/auth.js';
-
-const router = Router();
-
-router.post('/signin', login);
-router.post('/signup', createUser);
-
-export default router;
