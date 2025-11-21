30ea35b3d32f585743d440abaafdaf96b594c1dc
Fred Hobbs <FHobbs8030@gmail.com>
2025-11-20 17:53:41 -0800

    modify

--- routes/items.js
+++ routes/items.js
@@ -8,11 +8,22 @@ import {
   deleteItem,
 } from '../controllers/items.js';
 import validateObjectId from '../middlewares/validateObjectId.js';
+import Item from '../models/item.js';
 
 const router = Router();
 
 router.get('/', getItems);
 router.post('/', createItem);
+
+router.delete('/all', async (req, res, next) => {
+  try {
+    const result = await Item.deleteMany({});
+    res.send({ deletedCount: result.deletedCount });
+  } catch (err) {
+    next(err);
+  }
+});
+
 router.put('/:itemId/likes', validateObjectId('itemId'), likeItem);
 router.delete('/:itemId/likes', validateObjectId('itemId'), unlikeItem);
 router.delete('/:itemId', validateObjectId('itemId'), deleteItem);
