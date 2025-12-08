c055b415cd881cbbf0c40cdf88b312d01db32b1b
Fred Hobbs <FHobbs8030@gmail.com>
2025-12-07 19:55:02 -0800

    modify

--- routes/users.js
+++ routes/users.js
@@ -1,11 +1,11 @@
-﻿import { Router } from "express";
-import { getCurrentUser, getUserById, updateProfile } from "../controllers/users.js";
-import validateObjectId from "../middlewares/validateObjectId.js";
+﻿import { Router } from 'express';
+import { getCurrentUser, getUserById, updateUser } from '../controllers/users.js';
+import validateObjectId from '../middlewares/validateObjectId.js';
 
 const router = Router();
 
-router.get("/me", getCurrentUser);
-router.get("/:userId", validateObjectId, getUserById);
-router.patch("/me", updateProfile);
+router.get('/me', getCurrentUser);
+router.get('/:userId', validateObjectId, getUserById);
+router.patch('/me', updateUser);
 
 export default router;
