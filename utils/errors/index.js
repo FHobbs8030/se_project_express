03c1b4a078202828fe922961a82c18936003d0ee
Fred Hobbs <FHobbs8030@gmail.com>
2025-12-25 07:53:02 -0800

    fix: add custom error classes

---
+++ utils/errors/index.js
@@ -0,0 +1,6 @@
+import BadRequestError from './BadRequestError.js';
+import UnauthorizedError from './UnauthorizedError.js';
+import NotFoundError from './NotFoundError.js';
+import ConflictError from './ConflictError.js';
+
+export { BadRequestError, UnauthorizedError, NotFoundError, ConflictError };
