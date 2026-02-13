8add4f3f9eb4964dc960db6c43997675595f17a6
Fred Hobbs <FHobbs8030@gmail.com>
2026-02-12 21:53:10 -0800

    Refactor auth middleware: centralized UnauthorizedError handling

--- middlewares/auth.js
+++ middlewares/auth.js
@@ -1,19 +1,20 @@
 import jwt from 'jsonwebtoken';
+import UnauthorizedError from '../utils/errors/UnauthorizedError.js';
 
 const { JWT_SECRET = 'dev-secret' } = process.env;
 
-export default function auth(req, res, next) {
-  const token = req.cookies.jwt;
+export default function auth(req, _res, next) {
+  const { jwt: token } = req.cookies;
 
   if (!token) {
-    return res.status(401).send({ message: 'Authorization required' });
+    return next(new UnauthorizedError('Authorization required'));
   }
 
   try {
     const payload = jwt.verify(token, JWT_SECRET);
     req.user = payload;
     return next();
-  } catch {
-    return res.status(401).send({ message: 'Authorization required' });
+  } catch (err) {
+    return next(new UnauthorizedError('Authorization required'));
   }
 }
